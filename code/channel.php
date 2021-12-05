221ad7a9f5086cdda0af33e24baea6f805cdebf1
KrystianWiazowskiV2 <89790188+bouncychair@users.noreply.github.com>
2021-12-05 18:39:54 +0100

    Added changes to channel page

--- code/channel.php
+++ code/channel.php
@@ -1,6 +1,6 @@
+<?php require "connect.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,9 +9,29 @@
     <title>Channel</title>
 </head>
 <body>
-    <div class="header"> 
+    <div class="header">
         <img src="../img/logo1.png" alt="TocTic_logo">
         <h2 id="company_name">TocTic</h2>
-    </div> 
+    </div>
+    <div class="channel_header">
+        <div class="post_header">
+          <img id="channel_pic" src="../img/profileDef.png" alt="profilepic">
+          <!--<?php // echo $_POST['MainPic'] ?>-->
+        </div>
+        <div class="channel_name">
+            <h3>Channel Name</h3>        <!--<?php //echo $_POST['CName'] ?>-->
+        </div>
+        <div class="channel_description">
+          <h4>Description</h4>          <!--<?php //echo $_POST['Description'] ?>-->
+        </div>
+        <hr>
+    </div>
+    <div class="footer">
+      <img src="" alt="Main_menu">
+      <img src="" alt="Channels">
+      <img src="" alt="Notifications">
+      <img src="" alt="Add_post">
+      <img src="" alt="Profile">
+    </div>
 </body>
 </html>
