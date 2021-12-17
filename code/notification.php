1d2e7f2a74de0ce34b0cd7e620b6779dcfcede25
Polinalavender <91316935+Polinalavender@users.noreply.github.com>
2021-12-17 11:56:06 +0100

    Update notification.php

--- code/notification.php
+++ code/notification.php
@@ -1,51 +1,35 @@
-<?php include_once 'connect.php';?>
+<?php include_once 'connect.php';
+include_once 'utils.php';
+?>
 <!doctype html>
 <html lang="en">
   <head>
-    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <link href="Stylesheet.css" rel="stylesheet">
     <title>Notification</title>
   </head>
   <body>
-    <h1>New Notification</h1>
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
-        
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        
-      </ul>
+    <div class="header">
+        <img src="../img/logo1.png" alt="TocTic Logo" />
+        <h2>TocTic</h2>
+</div>
+    <h3>Latest Notifications</h3>
+    <div class="main-notifications">
+      <div class="box-notifications">
       <?php
-        $sql_get = mysqli_query($conn, "SELECT * FROM messages WHERE status = 0"); 
-        $count = mysqli_num_rows($sql_get);
-        echo $count;
-      ?>
-      
-    </div>
-  </div>
-</nav>
-    
+         $id = GetUserId($conn);
+         $query = "SELECT notifications.* FROM notifications, followed WHERE Notifications.ChannelId = followed.ChanelId AND followed.UserId = ? GROUP BY date";
+         $data = Query($conn, $query, "i", $id);
+         if ("SELECT Users.Id, Posts.id FROM `followed`, `Posts` WHERE Followed.UserId = ? AND Channels.UserId = Followed.UserId AND Post.id = ?"){
+           echo $data . "is created a post";
+         }
+         for ($i=0; $i < sizeof($data); $i++) { 
+          echo $data[$i]["Posts.id"];
+       }
+    ?> 
+    <?php
+    include "footer.php";
+    ?>
   </body>
 </html>
