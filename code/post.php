de93f0d907962453113f6352c8f0939f4d6957f3
pepehonourable <91469944+pepehonourable@users.noreply.github.com>
2021-12-22 21:45:36 +0100

    Add files via upload
    Moved styling to stylesheet
    Cleaned up code
    Added back button
    Added Error messages

--- code/post.php
+++ code/post.php
@@ -38,97 +38,52 @@ if (isset($_POST["submit"])) {
         <img src="../img/logo1.png" alt="TocTic Logo" />
         <h2>TocTic</h2>
     </div>
-    <style>
-    .comments{border: 3px ;margin: 0px auto 0px;padding: 15px 15px 5px 15px;max-width: 100%;height: auto;overflow: scroll;}
-    .comments li{display: block;margin-bottom: 5px;padding: 10px;padding-bottom: 10px;box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);}
-    .comments li:last-child{border-bottom: 0 dashed #ddd;margin-bottom:0px}
-    .comments span{color: #888;}
-    .insert_comment{width: 100%;
-        height:60px;
-    border: 1px solid white;
-    background: none;
-    padding: 5px 10px;
-    border-radius:5px;
-    color: white;
-    margin-top: 15px;
-    resize: none;
-}
-    .insert_comment ::placeholder{color:white;}  
-    .error_comments {
-    background-color: rgb(241, 74, 74);
-    padding: 10px;
-    margin: 10px;
-    text-align: center;
-}   
-    
-    .comments_button{
-    margin-top: 15px;
-    margin-bottom: 15px;
-    border-radius: 20px;
-    border: 1px solid #FF4B2B;
-    background-color: #FF4B2B;
-    color: #FFFFFF;
-    font-size: 12px;
-    font-weight: bold;
-    padding: 12px 45px;
-    letter-spacing: 1px;
-    text-transform: uppercase;
-}
-    .comments_form{
-    background-color: #182952;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    padding: 0 20px;
-    text-align: center;
-}
-</style>
-<div class="post">
-    <div class="post_header">
-        <?php
-        $sql = "SELECT * FROM `Users` WHERE id=?";
-        $row = Query($conn, $sql, "i", $id);  
-        echo "<img src='../uploads/".$row[0]['ProfilePicture']."'>";
-        ?>
-        <p><?php echo $row[0]['Username'];?></p>  
-    </div>
-    <div class="post_caption">
-        <?php
-        $sql = "SELECT * FROM `Posts` WHERE id=?";
-        $row = Query($conn, $sql, "i", $post);
-        ?>
-        <p><?php echo $row[0]['Caption'];?></p>
-    </div>
-    <div>
-        <?php
-        $sql = "SELECT * FROM `Posts` WHERE id=?";
-        $row = Query($conn, $sql, "i", $post);
-        echo "<img src='../uploads/".$row[0]['ImageName']."' >";
-        ?>
-    </div>
-    <div class="comments">
-       <?php
-       $sql = "SELECT * FROM `Comments` c, Users u WHERE PostId=? && c.UserId=u.id ORDER BY `Date` DESC";
-       $row = Query($conn, $sql, "i", $post);     
-       if (sizeof($row) == 0) {
-        echo "<h3>No comments yet!</h3>";
-    }
-    else {         
-       for ($i=0; $i < sizeof($row); $i++){
-           echo "<li><b style='color:white'>" .$row[$i]['Username'] . "<b>  -  " .$row[$i]['Date'] . " <br><h3> " .$row[$i]['Text'] . "</h3></li>";
+    <div class="post_comments">
+        <div class="post_comments_header">
+            <?php
+            $sql = "SELECT * FROM `Users` WHERE id=?";
+            $row = Query($conn, $sql, "i", $id);  
+            echo "<img src='../uploads/".$row[0]['ProfilePicture']."'>";
+            ?>
+            <p><?php echo $row[0]['Username'];?></p>
+            <div class="back_button">
+                <a href="../code/main.php"><img src="../img/Project2_back_arrow_big.png"></a>
+            </div> 
+        </div>
+        <div class="post_caption">
+            <?php
+            $sql = "SELECT * FROM `Posts` WHERE id=?";
+            $row = Query($conn, $sql, "i", $post);
+            ?>
+            <p><?php echo $row[0]['Caption'];?></p>
+        </div>
+        <div>
+            <?php
+            $sql = "SELECT * FROM `Posts` WHERE id=?";
+            $row = Query($conn, $sql, "i", $post);
+            echo "<img src='../uploads/".$row[0]['ImageName']."' >";
+            ?>
+        </div>
+        <div class="comments">
+           <?php
+           $sql = "SELECT * FROM `Comments` c, Users u WHERE PostId=? && c.UserId=u.id ORDER BY `Date` DESC";
+           $row = Query($conn, $sql, "i", $post);     
+           if (sizeof($row) == 0) {
+            echo "<h3>No comments yet!</h3>";
+        }
+        else {         
+           for ($i=0; $i < sizeof($row); $i++){
+               echo "<li><b style='color:white'>" .$row[$i]['Username'] . "<b>  -  " .$row[$i]['Date'] . " <br><h3> " .$row[$i]['Text'] . "</h3></li>";
+           }
        }
-   }
-   ?>
-</div>
-<br>
-<br>
+       ?>
+   </div>
 </div>
 <div class="add_comment">
     <form action="post.php?PostId=<?php echo $post ?>" class="comments_form" method="POST">
-    <textarea class="insert_comment" type="text" id="comment" name="comments" placeholder="Comment here"></textarea>
-    <button type="submit" name="submit" class="comments_button">Add Comment</button>   
-</form>
+        <textarea class="insert_comment" type="text" id="comment" name="comments" placeholder="Comment here"></textarea>
+        <button type="submit" name="submit" class="comments_button">Add Comment</button>   
+    </form>
 </div>
 </body>
 </html>
