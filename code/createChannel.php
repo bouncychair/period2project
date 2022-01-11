64c989eea057b247d6ddf7db125162e763d4b95e
radoychev <62029581+radoychev@users.noreply.github.com>
2022-01-11 16:00:13 +0100

    Update createChannel.php

--- code/createChannel.php
+++ code/createChannel.php
@@ -17,7 +17,7 @@ $id = GetUserId($conn);
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="Stylesheet.css">
-  <title>Add post</title>
+  <title>Create Channel</title>
 </head>
 
 
